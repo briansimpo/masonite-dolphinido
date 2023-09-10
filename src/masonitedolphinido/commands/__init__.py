c743106a8c587fa0e54736b5ad01d6fbc86f34ec
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-09-11 00:29:01 +0200

    development

--- src/masonitedolphinido/commands/__init__.py
+++
@@ -1,5 +0,0 @@
-from .FingerprintCommand import FingerprintCommand
-from .RadioCommand import RadioCommand
-from .RecogFileCommand import RecogFileCommand
-from .RecogMicCommand import RecogMicCommand
-from .RecogRadioCommand import RecogRadioCommand
