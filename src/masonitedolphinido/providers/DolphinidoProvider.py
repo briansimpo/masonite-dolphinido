68aa029916f6ba53c6bc2a17df6e551d9dd1f6b4
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-09-11 13:30:51 +0200

    Update DolphinidoProvider.py

--- src/masonitedolphinido/providers/DolphinidoProvider.py
+++ src/masonitedolphinido/providers/DolphinidoProvider.py
@@ -10,13 +10,13 @@ class DolphinidoProvider(PackageProvider):
     def configure(self):
         self.root("masonitedolphinido")\
         .name("masonitedolphinido")\
-        .config("config/fingerprint.py", publish=True)\
         .migrations(
             "migrations/create_audios_table.py", 
             "migrations/create_audio_fingerprints_table.py"
         )
     
     def register(self):
+        self.configure()
         self.application.make('commands')\
         .add(Fingerprint())\
         .add(Radio())\
