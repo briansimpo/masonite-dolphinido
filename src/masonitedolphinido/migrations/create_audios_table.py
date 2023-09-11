350872d23f33d18517632c106b8108b57e2813e7
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-09-11 13:42:39 +0200

    Update create_audios_table.py

--- src/masonitedolphinido/migrations/create_audios_table.py
+++ src/masonitedolphinido/migrations/create_audios_table.py
@@ -10,8 +10,10 @@ class CreateAudiosTable(Migration):
         """
         with self.schema.create("audios") as table:
             table.uuid('id').primary()
-            table.string('hash_id').unique().nullable()
-            table.integer('hash_count').nullable()
+            table.string('hash').unique()
+            table.binary('metadata').nullable()
+            table.integer('hashcount').nullable()
+            
             table.timestamps()
 
     def down(self):
